c2e6112398f27a0eb0ee0827895cfdf611ebccf7
გიორგი <giokaxo@gmail.com>
2021-07-31 16:51:23 +0400

    add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import store from '../store';
 
-import Home from '../views/Home.vue'
+import Home from '../views/Home.vue';
 
-const routes = [{
-    path: '/',
-    name: 'Home',
-    component: Home
-},
-{
-    path: '/about-us',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-        import('../views/About.vue'),
-    meta: {
-        layout: 'Content'
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about-us',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import('../views/About.vue'),
+        meta: {
+            layout: 'Content'
+        },
+    },
+    {
+        path: '/:match(.*)',
+        component: () => import('../views/NotFound.vue')
     }
-}
-]
+];
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
@@ -30,6 +35,6 @@ const router = createRouter({
 router.beforeEach((to) => {
     const layout = to?.meta?.layout || 'DefaultLayout';
     store.commit('SET_LAYOUT', layout);
-})
+});
 
-export default router
+export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center">
+    <h2 class="py-5">Page not found</h2>
+    <router-link to="/">Return To Home Page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+};
+</script>
