7cc0e58587dff5cffccdfd2dff8e143f9578a76b
Sandro <sandro1211@gmail.com>
2021-08-05 19:51:43 +0400

    add dropdown component

---
+++ src/components/FormControls/Dropdown.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <select v-model="sortBy">
+    <option value="asc">A-Z</option>
+    <option value="desc">Z-A</option>
+  </select>
+</div>
+</template>
+
+<script>
+export default{
+    emits: ['update:modelValue'],
+    props: {
+        modelValue: {
+            type: String,
+            default: ''
+        }
+    },
+data(){
+    return {
+sortBy:''
+    };
+
+
+},
+watch: {
+    sortBy(value) {
+        // this.$emit('input', value); //For Vue 2
+        this.$emit('update:modelValue', value);
+    },
+    modelValue: {
+        immediate:true,
+        handler(value) {
+this.sortBy = value;
+        }
+    }
+}
+
+
+
+};
+</script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <div class="row mt-3">
-      <div class="col-3">test</div>
+      <div class="col-3">
+        <Dropdown v-model="sortBy"/>
+      </div>
       <div class="col-9">
         <div class="row">
           <div class="col-3 mb-3" v-for="item in productList" :key="item.id">
@@ -18,16 +20,19 @@
 
 <script>
 import ProductItem from '@/components/ProductItem.vue';
+import Dropdown from '@/components/FormControls/Dropdown.vue';
 import axios from 'axios';
 
 export default {
   components: {
-    ProductItem
+    ProductItem,
+    Dropdown
   },
   data() {
     return {
       products: [],
-      itemsToShow: 8
+      itemsToShow: 8,
+      sortBy:'asc'
     };
   },
   mounted() {
@@ -37,13 +42,15 @@ export default {
   },
   computed: {
     productList() {
+
       return this.products.slice(0, this.itemsToShow);
     }
   },
   methods: {
     showMore() {
       this.itemsToShow += 8;
-    }
+    },
+    
   }
 };
 </script>
